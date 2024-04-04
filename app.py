6af0553ab705f02bad3059ae4c25e34c20c437c9
FriendlyOnion <63168062+friendlyonion@users.noreply.github.com>
2024-04-04 13:41:17 -0500

    t

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, render_template, jsonify
-from flask import url_for, request
+from flask import Flask, render_template, jsonify, request
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
