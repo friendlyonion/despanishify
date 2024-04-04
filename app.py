01fff9236aba0223f1494955d09ed102f38d59fb
FriendlyOnion <63168062+friendlyonion@users.noreply.github.com>
2024-04-04 13:58:56 -0500

    t

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
-# the associated function.t
+# the associated function.tt
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def index():
